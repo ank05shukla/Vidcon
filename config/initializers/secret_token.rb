181e139058d1cb592d94624b4ab7f0de628960d7
WebAppFrameworks <wafuser@wafuser-VirtualBox.(none)>
2012-12-16 16:51:27 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidcon::Application.config.secret_token = '17e87b16caee6b470ee4742acb148e19a191302126591b463274a65acc02398156ee3767cb5772f175d703d12815acd738452d9b73b3725a8cbcd462272e3872'
